4861427911850dc9f313cff4c2fdc1a77f617483
Sanya <nikulinsanya@gmail.com>
2015-07-05 19:18:05 +0200

    new date ranges

--- application/views/Reports/Submissions.php
+++ application/views/Reports/Submissions.php
@@ -1,43 +1,48 @@
 <?php $week = strtotime('this week', strtotime('this week') > time() ? strtotime('yesterday') : time());?>
-<form action="" method="get" class="auto-submit">
-<div class="col-xs-4">
-<?=Form::input('start', Arr::get($_GET, 'start', date('d-m-Y', $week)), array('class' => 'form-control datepicker', 'placeholder' => 'Start date', 'id' => 'start'))?>
-</div>
-<div class="col-xs-4">
-<?=Form::input('end', Arr::get($_GET, 'end', date('d-m-Y')), array('class' => 'form-control datepicker', 'placeholder' => 'End date', 'id' => 'end'))?>
-</div>
-<div class="col-xs-4">
-    <button type="button" class="btn btn-info date-range" data-suffix="" data-start="<?=date('d-m-Y', strtotime('first day of this month'))?>" data-end="<?=date('d-m-Y')?>">This month</button>
-    <button type="button" class="btn btn-info date-range" data-suffix="" data-start="<?=date('d-m-Y', strtotime('first day of last month'))?>" data-end="<?=date('d-m-Y', strtotime('last day of last month'))?>">Last month</button>
-    <button type="button" class="btn btn-info date-range" data-suffix="" data-start="<?=date('d-m-Y', $week)?>" data-end="<?=date('d-m-Y')?>">This week</button>
-    <button type="button" class="btn btn-info date-range" data-suffix="" data-start="<?=date('d-m-Y', strtotime('-1 week', $week))?>" data-end="<?=date('d-m-Y', strtotime('-1 day', $week))?>">Last week</button>
-</div>
-<div class="clearfix">&nbsp;</div>
-<div class="col-xs-4">
-<?=Form::input('app-start', Arr::get($_GET, 'app-start'), array('class' => 'form-control datepicker', 'placeholder' => 'Start date (Approved)', 'id' => 'app-start'))?>
-</div>
-<div class="col-xs-4">
-<?=Form::input('app-end', Arr::get($_GET, 'app-end'), array('class' => 'form-control datepicker', 'placeholder' => 'End date (Approved)', 'id' => 'app-end'))?>
-</div>
-<div class="col-xs-4">
-    <button type="button" class="btn btn-info date-range" data-suffix="app" data-start="<?=date('d-m-Y', strtotime('first day of this month'))?>" data-end="<?=date('d-m-Y')?>">This month</button>
-    <button type="button" class="btn btn-info date-range" data-suffix="app" data-start="<?=date('d-m-Y', strtotime('first day of last month'))?>" data-end="<?=date('d-m-Y', strtotime('last day of last month'))?>">Last month</button>
-    <button type="button" class="btn btn-info date-range" data-suffix="app" data-start="<?=date('d-m-Y', $week)?>" data-end="<?=date('d-m-Y')?>">This week</button>
-    <button type="button" class="btn btn-info date-range" data-suffix="app" data-start="<?=date('d-m-Y', strtotime('-1 week', $week))?>" data-end="<?=date('d-m-Y', strtotime('-1 day', $week))?>">Last week</button>
-    <button type="button" class="btn btn-danger date-range" data-suffix="app" data-start="" data-end="">Clear</button>
-</div>
-<div class="clearfix">&nbsp;</div>
-<?php if (Group::current('allow_assign')):?>
-<div class="col-xs-12">
-<select name="company" class="selectize">
-    <option value="">All contractors</option>
-    <?php foreach ($companies as $key => $value):?>
-    <option value="<?=$key?>" <?=$key == Arr::get($_GET, 'company') ? 'selected' : ''?>><?=$value?></option>
-    <?php endforeach;?>
-</select>
-</div>
-<div class="clearfix">&nbsp;</div>
-<?php endif;?>
+<form action="" method="get" class="auto-submit report-form-container">
+
+    <div class="filter-info-container">
+        <label class="date-range-label">Date range: </label>
+        <span class="date-range-container">
+            <div class="daterange" class="pull-right" data-start="start" data-end="end" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                <span></span> <b class="caret"></b>
+            </div>
+        </span>
+
+        <div class="clearfix">&nbsp;</div>
+
+
+        <label class="date-range-label">Date range(Approved) :</label>
+         <span class="date-range-container">
+            <div class="daterange" class="pull-right"  data-start="app-start" data-end="app-end" style="background: #fff; cursor: pointer; padding: 5px 10px; border: 1px solid #ccc">
+                <i class="glyphicon glyphicon-calendar fa fa-calendar"></i>
+                <span></span> <b class="caret"></b>
+            </div>
+        </span>
+
+
+        <div class="clearfix">&nbsp;</div>
+        <?php if (Group::current('allow_assign')):?>
+        <label class="date-range-label filter-select-label">Contractors :</label>
+        <span class="filter-select-container">
+            <select name="company" class="selectize">
+                <option value="">All contractors</option>
+                <?php foreach ($companies as $key => $value):?>
+                <option value="<?=$key?>" <?=$key == Arr::get($_GET, 'company') ? 'selected' : ''?>><?=$value?></option>
+                <?php endforeach;?>
+            </select>
+        </span>
+        <?php endif;?>
+    </div>
+
+
+
+<?=Form::hidden('start', Arr::get($_GET, 'start', date('d-m-Y', $week)), array('class' => 'form-control datepicker', 'placeholder' => 'Start date', 'id' => 'start'))?>
+<?=Form::hidden('end', Arr::get($_GET, 'end', date('d-m-Y')), array('class' => 'form-control datepicker', 'placeholder' => 'End date', 'id' => 'end'))?>
+
+<?=Form::hidden('app-start', Arr::get($_GET, 'app-start'), array('class' => 'form-control datepicker', 'placeholder' => 'Start date (Approved)', 'id' => 'app-start'))?>
+<?=Form::hidden('app-end', Arr::get($_GET, 'app-end'), array('class' => 'form-control datepicker', 'placeholder' => 'End date (Approved)', 'id' => 'app-end'))?>
 </form>
 <h2>Total <?=count($submissions)?> job(s) found:</h2>
 <div class="upload-buttons text-right">
@@ -47,9 +52,7 @@
 <table class="table table-hover">
     <?php foreach ($submissions as $job => $list):?>
     <tr>
-        <th colspan="<?=Group::current('allow_assign') ? 6 : 5?>"><a href="<?=URL::base()?>search/view/<?=$job?>"><?=$job?></a></th>
-    </tr>
-    <tr>
+        <th>Ticket ID</th>
         <th>Submission date</th>
         <th>Approval date</th>
         <th>User</th>
@@ -59,6 +62,7 @@
     </tr>
     <?php foreach ($list as $submission): $key = substr($submission['key'], 5); $status = Arr::get($submission, 'active', 0);?>
     <tr class="<?=$status > 0 ? 'bg-warning' : ($status < 0 ? 'bg-success' : 'bg-danger')?>">
+        <td><a href="<?=URL::base()?>search/view/<?=$job?>"><?=$job?></a></td>
         <td><?=date('d-m-Y H:i', $submission['update_time'])?></td>
         <td><?=isset($submission['process_time']) ? date('d-m-Y H:i', $submission['process_time']) : ''?></td>
         <td><?=User::get($submission['user_id'], 'login')?></td>
