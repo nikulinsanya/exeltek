38a8660d6145292638d474e035a31ffb593d2136
Alex Galkin <ag@savvyled.pl>
2015-07-13 22:49:03 +0200

    Batch edit api - get

---
+++ application/classes/Controller/Search/Batch.php
@@ -0,0 +1,56 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Search_Batch extends Controller
+{
+    public function action_index()
+    {
+        $action = $this->request->param('id');
+
+        $result = array();
+
+        switch ($action) {
+            case 'get':
+                $columns = DB::select('id')->from('job_columns')->where('editable', '=', 1)->execute()->as_array(NULL, 'id');
+                $data = array();
+
+                foreach ($columns as $column) {
+                    $value = array(
+                        'id' => $column,
+                        'name' => Columns::get_name($column),
+                    );
+                    $type = Columns::get_type($column);
+
+                    if (substr($type, 0, 4) == 'enum') {
+                        $id = substr($type, 5);
+                        $type = Enums::is_multi($id) ? 'multi' : 'list';
+                        $value['values'] = array_values(Enums::get_values($id));
+                    }
+                    $value['type'] = $type ? : 'string';
+
+                    $result['columns'][] = $value;
+
+                    $data[] = 'data.' . $column;
+                }
+
+                $ids = Arr::get($_GET, 'id', array());
+                $values = array();
+                if ($ids) {
+                    $query = array('_id' => array('$in' => $ids));
+                    if (!Group::current('show_all_jobs'))
+                        $query['companies'] = intval(User::current('company_id'));
+
+                    $jobs = Database_Mongo::collection('jobs')->find($query, $data);
+                    foreach ($jobs as $job) {
+                        $job['id'] = $job['_id'];
+                        unset($job['_id']);
+                        $values[] = $job;
+                    }
+                }
+                $result['jobs'] = $values;
+                break;
+        }
+
+        die(json_encode($result));
+    }
+
+}
