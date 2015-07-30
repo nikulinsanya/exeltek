6c52ba829fa7a18cbc9e80ccda9f0d31ec88c15a
Alex Galkin <ag@savvyled.pl>
2015-07-30 02:09:18 +0200

    Time Machine update

--- application/classes/Controller/Timemachine.php
+++ application/classes/Controller/Timemachine.php
@@ -9,7 +9,7 @@ class Controller_TimeMachine extends Controller
     public function action_index() {
         $id = strval(Arr::get($_GET, 'id'));
 
-        $job = Database_Mongo::collection('jobs')->findOne(array('_id' => $id), array('_id' => 1));
+        $job = Database_Mongo::collection('jobs')->findOne(array('_id' => $id), array('data' => 1));
 
         if (!$job) throw new HTTP_Exception_404('Not found');
 
@@ -39,10 +39,20 @@ class Controller_TimeMachine extends Controller
 
         if ($new) $new['$set']['last_update'] = $archive['update_time'];
 
+        $submissions = array();
+        $result = Database_Mongo::collection('submissions')->find(array('job_key' => $id, 'process_time' => array('$gt' => $archive['update_time'])));
+
+        foreach ($result as $item) if (Arr::path($job, $item['key'], '') != $item['value'])
+            $submissions[] = $item['_id'];
+
+        Database_Mongo::collection('submissions')->update(array('_id' => array('$in' => $submissions)), array('$unset' => array('process_time' => 1), '$set' => array('active' => 1)));
         Database_Mongo::collection('jobs')->update(array('_id' => $id), $new);
         Database_Mongo::collection('archive')->remove(array('_id' => array('$in' => $ids)));
 
-        Messages::save('Ticket ' . $id . ' successfully rolled back to ' . date('d-m-Y H:i', $archive['update_time']) . '. ' . count($ids) . ' changes were removed.');
+        $message = '';
+        if (count($ids)) $message .= count($ids) . ' changes were removed. ';
+        if (count($submissions)) $message .= count($submissions) . ' submissions were unapproved. ';
+        Messages::save('Ticket ' . $id . ' successfully rolled back to ' . date('d-m-Y H:i', $archive['update_time']) . '. ' . $message);
 
         die(json_encode(array('success' => true)));
     }
