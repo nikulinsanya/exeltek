e139187fb361f9396117b652652031fe5c3fb336
Alex Galkin <ag@savvyled.pl>
2015-08-20 00:01:38 +0200

    dummy controller

---
+++ application/classes/Controller/Search/Update.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Search_Update extends Controller
+{
+    public function action_index()
+    {
+        dir('zzz');
+    }
+}
