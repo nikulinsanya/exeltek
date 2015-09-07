74ccfc75b8579c452e5619f0faa1aea16b12bd45
Sanya <nikulinsanya@gmail.com>
2015-09-07 23:23:30 +0200

    form to view

---
+++ application/classes/Controller/Form.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class Controller_Form extends Controller {
+
+    public function action_build()
+    {
+        $view = View::factory('Form');
+
+        $this->response->body($view);
+    }
+}
