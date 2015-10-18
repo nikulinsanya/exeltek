caa643df554500ce1343db52d55c9f9a78e6488d
Alex Galkin <ag@savvyled.pl>
2015-10-19 00:59:14 +0200

    removed unused code

--- application/classes/Controller/Form.php
+++ application/classes/Controller/Form.php
@@ -14,16 +14,6 @@ class Controller_Form extends Controller {
         $this->response->body($view);
     }
 
-    public function action_build()
-    {
-        $info = Database_Mongo::collection('api')->findOne(array('_id' => new MongoId('5601c4b7c92cb670368b4690')));
-
-        $view = View::factory('Form')
-            ->set('json', json_encode($info['data']));
-
-        $this->response->body($view);
-    }
-
     public function action_print() {
         $id = Arr::get($_GET, 'id');
         $form = Database_Mongo::collection('forms-data')->findOne(array('_id' => new MongoId($id)));
@@ -188,8 +178,4 @@ class Controller_Form extends Controller {
         )));
     }
 
-    public function action_generate()
-    {
-        Database_Mongo::collection('api')->insert(array('data' => json_decode(file_get_contents('php://input'), true)));
-    }
 }
