a88d3cff398db1e7724921dcc345ca9903989ba4
Alex Galkin <ag@savvyled.pl>
2015-12-13 19:53:26 +0100

    Enums editor

--- application/views/Security/Enums.php
+++ application/views/Security/Enums.php
@@ -5,7 +5,7 @@
         <th>Multi-values</th>
     </tr>
     <?php foreach ($items as $item):?>
-    <tr>
+    <tr data-id="<?=$item['id']?>">
         <td><button class="btn btn-warning edit-enum" data-id="<?=$item['id']?>">Edit</button></td>
         <td><?=$item['name']?></td>
         <td><span class="glyphicon glyphicon-<?=$item['allow_multi'] ? 'ok text-success' : 'remove text-danger'?>"></span></td>
