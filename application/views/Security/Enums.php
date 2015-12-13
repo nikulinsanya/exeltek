d366b1cfb5b774578dc7c1e26a5c5569816cdd27
Alex Galkin <ag@savvyled.pl>
2015-12-13 19:52:48 +0100

    bugfix

---
+++ application/views/Security/Enums.php
@@ -0,0 +1,14 @@
+<table class="table">
+    <tr>
+        <th><button class="btn btn-success edit-enum" data-id="0">Add</button></th>
+        <th>Name</th>
+        <th>Multi-values</th>
+    </tr>
+    <?php foreach ($items as $item):?>
+    <tr>
+        <td><button class="btn btn-warning edit-enum" data-id="<?=$item['id']?>">Edit</button></td>
+        <td><?=$item['name']?></td>
+        <td><span class="glyphicon glyphicon-<?=$item['allow_multi'] ? 'ok text-success' : 'remove text-danger'?>"></span></td>
+    </tr>
+    <?php endforeach;?>
+</table>
