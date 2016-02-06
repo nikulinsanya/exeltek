2029422cfb7275a081cd43a69f14939c2fe7f356
Sanya <nikulinsanya@gmail.com>
2016-02-07 00:04:22 +0100

    fixed modal

--- application/views/Content.php
+++ application/views/Content.php
@@ -185,7 +185,7 @@
       </div>
   </div>
   <div class="modal fade" id="modal-download" tabindex="-1" role="dialog">
-      <div class="modal-dialog" role="document">
+      <div class="modal-dialog modal-sm" role="document">
           <div class="modal-content">
               <div class="modal-header">
                   <h4 class="modal-title">Preparing attachments...</h4>
