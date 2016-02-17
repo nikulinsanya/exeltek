8fbf18304079672e5b5cc59965385d83a9ae31da
Sanya <nikulinsanya@gmail.com>
2016-02-17 21:11:07 +0100

    fixed select

--- application/views/Forms/Builder.php
+++ application/views/Forms/Builder.php
@@ -87,7 +87,7 @@
 </div>
 
 <div class="modal fade"  id="configTable" tabindex="-1" role="dialog">
-    <div class="modal-dialog" role="document" style="width:500px;">
+    <div class="modal-dialog" role="document" style="width:600px;">
         <div class="modal-content">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
